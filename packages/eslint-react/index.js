151e57d617053b24288fa5233d329374c8d46253
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-07-20 15:27:15 +0200

    feat(eslint-react)!: add a11y support

--- packages/eslint-react/index.js
+++ packages/eslint-react/index.js
@@ -1,6 +1,14 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['@equinor/eslint-config-fusion', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    '@equinor/eslint-config-fusion',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:styled-components-a11y/recommended',
+  ],
+  plugins: [
+    'styled-components-a11y'
+  ],
   settings: {
     react: {
       version: 'detect',
